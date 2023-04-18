e981f828db1ea9dd5eb414e85303c2fd618c2e80
Hanasuj <124556657+Hanasuj@users.noreply.github.com>
2023-04-18 21:46:46 +0200

    Add files via upload

--- sorting.py
+++ sorting.py
@@ -10,6 +10,24 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as file:
+        datas = []
+        l1 = []
+        l2 = []
+        l3 = []
+        all_data = file.readlines()
+        data_lines = [thing.strip() for thing in all_data]
+        titles = (data_lines.pop(0)).split(",")
+        for idx in range(len(data_lines)):
+            thingies = data_lines[idx].split(",")
+            l1.append(thingies[0])
+            l2.append(thingies[1])
+            l3.append(thingies[2])
+        datas.append(l1)
+        datas.append(l2)
+        datas.append(l3)
+        data = dict(zip(titles, datas))
+        return data
 
 
 def main():
@@ -18,3 +36,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    read_data("numbers.csv")
